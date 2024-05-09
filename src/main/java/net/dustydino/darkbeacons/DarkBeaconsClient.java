58a19a5af6d7c8908901db2b66c72d1899e588ac
Dus7yDINO <daniel.g.archer@gmail.com>
2024-05-09 23:26:52 +0100

    Initial commit

---
+++ src/main/java/net/dustydino/darkbeacons/DarkBeaconsClient.java
@@ -0,0 +1,10 @@
+package net.dustydino.darkbeacons;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DarkBeaconsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/dustydino/darkbeacons/DarkBeaconsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.dustydino.darkbeacons;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DarkBeaconsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
